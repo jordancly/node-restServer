7ab179415006e4872ee964e11a13100c3257623d
JordanCly <fjordancly@gmail.com>
2018-05-04 11:11:16 -0500

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -11,38 +14,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-})
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
+app.use(require('./routes/usuario'));
 
-    if (body.nombre == undefined) {
-        res.status(400).json({
-            ok: "false",
-            mensaje: "El nombre es necesario"
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
 
-})
 
-app.put('/usuario/:id', function(req, res) {
+mongoose.connect(process.env.URLDB, (err, res) => {
 
-    let id = req.params.id;
-    res.json({
-        id
-    });
-})
+    if (err) throw err;
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-})
+    console.log('Base de datos ONLINE');
+
+});
+
+
 
 app.listen(process.env.PORT, () => {
-    console.log("Escuchando en el puerto ", process.env.PORT);
-})
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
